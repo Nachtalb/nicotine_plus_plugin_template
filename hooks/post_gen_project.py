eb6aab3301ff2c5c7db6b5c75972893b79513c9a
Nachtalb <info@nachtalb.io>
2021-09-05 01:28:13 +0200

    Add plugin core as submodule

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,15 @@
 import subprocess
 
 subprocess.call(['git', 'init'])
-subprocess.call(['git', 'add', '.'])
-subprocess.call(['git', 'commit', '-m', 'initial commit'])
+
 {% if cookiecutter.github_remote == 'SSH' %}
 subprocess.call(['git', 'remote', 'add', 'origin', 'git@github.com:{{cookiecutter.github_username}}/{{cookiecutter.github_reponame}}.git'])
+subprocess.call(['git', 'submodule', 'add', 'git@github.com:Nachtalb/np_plugin.core.git', '{{cookiecutter.module_name}}/core/'])
 {% else %}
 subprocess.call(['git', 'remote', 'add', 'origin', 'https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.github_reponame}}.git'])
+subprocess.call(['git', 'submodule', 'add', 'https://github.com/Nachtalb/np_plugin.core.git', '{{cookiecutter.module_name}}/core/'])
 {% endif %}
+
+subprocess.call(['git', 'submodule', 'update', '--init', '--recursive'])
+subprocess.call(['git', 'add', '.'])
+subprocess.call(['git', 'commit', '-m', 'initial commit'])
